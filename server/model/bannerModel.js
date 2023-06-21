3f51d846f82664b574c72afa8ced8d24464b5ad2
jithin <jithinjolly15@gmail.com>
2023-06-21 10:15:23 +0530

    banner model creatd

---
+++ server/model/bannerModel.js
@@ -0,0 +1,30 @@
+const mongoose=require('mongoose')
+
+const bannerSchema= new mongoose.Schema({
+    name:{
+        type:String,
+        require:true
+    },
+    description :{
+        type:String,
+        require:true
+    },
+    photo:[{
+        type:String,
+        require:true,
+       
+       
+    }],
+    date:{
+        type:Date,
+        require:true
+    },
+    status:{
+        type:Boolean,
+       default:false
+    }
+    
+})
+const Banner= new mongoose.model("banners",bannerSchema)
+
+module.exports=Banner
